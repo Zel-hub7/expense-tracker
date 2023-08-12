b8b52c526e917e376570951e87d323443edfbdb9
zel-hub7 <zelalemyohannes62@gmail.com>
2023-08-12 02:21:37 -0700

    Create the setup

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import ExpenseItem from "./Components/ExpenseItem";
+function App() {
+  return (
+    <div>
+       <h1>Lets start Building</h1>
+       <ExpenseItem />
+    </div>
+  )
+}
+export default App;
---
+++ src/Components/ExpenseItem.js
@@ -0,0 +1,8 @@
+function ExpenseItem() {
+    return(
+        <div>
+            <h1>This is my expense item list</h1>
+        </div>
+    )
+}
+export default ExpenseItem;
